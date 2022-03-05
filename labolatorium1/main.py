c40c9a4fe039a68a7eb3a9639c516af66f875ed7
zbrzezna-wioleta <100282558+zbrzezna-wioleta@users.noreply.github.com>
2022-03-05 18:33:49 +0100

    Add files via upload

---
+++ labolatorium1/main.py
@@ -0,0 +1,33 @@
+# import sys
+# import math
+# print(sys.version)
+# a = 'informatyka ogólna grupa 4'
+# print(a)
+#
+# b=5
+# print(b)
+#
+# c =5.4
+# print(c)
+#
+# d=3+2j
+# print(d)
+#
+# #usuniecei
+# #del a
+# print(a)
+#
+# #komentarz
+#
+# e, f=3.14, 5
+# print(e+f)
+#
+# g =2
+# print(f//g)
+# print(f**g)
+# print(math.pow(f, g)) podnoszenie do potegi gdzie g to wykladnik
+
+a =5
+b=3
+c= 5-3
+print("wynik działania %(z1)d - %(z2)d = %(z3)d" %{'z1':a, 'z2':b 'z3':c })
