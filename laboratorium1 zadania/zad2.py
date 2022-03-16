254b1fd1fd7ead979f2340fdd2b9eeedc9db8176
zbrzezna-wioleta <100282558+zbrzezna-wioleta@users.noreply.github.com>
2022-03-16 12:41:14 +0100

    Update zad2.py

--- laboratorium1 zadania/zad2.py
+++ laboratorium1 zadania/zad2.py
@@ -1,5 +1,5 @@
 #Zad2. Stwórz skrypt kalkulator, w którym wykorzystać wszystkie podstawowe działania arytmetyczne
-
+# I sposób
 a=10
 b=2
 
@@ -17,3 +17,29 @@ print(mnozenie)
 
 potega=a**b
 print(potega)
+
+# II sposób
+
+znak = input("Podaj znak: /,+,%,^,-,*")
+a = input("Podaj liczbe: ")
+b = input("Podaj liczbe: ")
+a = float(a)
+b = float(b)
+if znak == '+':
+    print("Suma: ", a + b)
+elif znak == '-':
+    print("Roznica = ", a - b)
+elif znak == '^':
+    if (a == 0 & b == 0):
+        print("symbol nieoznaczony")
+    else:
+        print("Potega: ", a ** b)
+elif znak == '*':
+    print("Iloczyn: ", a * b)
+elif znak == '/':
+    if (b == 0):
+        print("Nie dzielimy przez 0!")
+    else:
+        print("Dzielenie", a / b)
+else:
+    print("Nieprawidlowy znak")
