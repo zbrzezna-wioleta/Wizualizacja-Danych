e7607fe6f3b24d8a5f0bdf2732c27c6f8f0b6c98
zbrzezna-wioleta <100282558+zbrzezna-wioleta@users.noreply.github.com>
2022-03-16 12:45:58 +0100

    Update zad9.py

--- laboratorium1 zadania/zad9.py
+++ laboratorium1 zadania/zad9.py
@@ -1,10 +1,9 @@
 # Zad.9 Napisz skrypt, w którym zadeklarujesz zmienne typu: string, float i szestnastkowe. Następnie
 # wyświetl je wykorzystując odpowiednie formatowanie
 
-x = hex(16)
-z = 'informatyka'
-y = 3.79
-print("nazwa przedmiotu: %s, zmienna szesnastkowa: %s, zmienna float: %f" % (z, x, y))
-
+val1 = "Ala ma kota"
+val2 = 2.453
 liczba_szesnastkowa=0x1f
+print("Zmienna typu string: %(zm1)s" %{'zm1':val1})
+print("Zmienna typu float: %(zm1)f"%{'zm1':val2})
 print('{0:x}'.format(liczba_szesnastkowa))
