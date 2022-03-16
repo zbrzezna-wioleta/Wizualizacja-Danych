0a4112e02d46b8859c550bed2ca14c4c5ce07965
zbrzezna-wioleta <100282558+zbrzezna-wioleta@users.noreply.github.com>
2022-03-16 12:39:24 +0100

    Update zad3.py

--- laboratorium1 zadania/zad3.py
+++ laboratorium1 zadania/zad3.py
@@ -1,1 +1,16 @@
 #Zad3. Napisz skrypt, w którym stworzysz operatory przyrostkowe dla operacji: +, -, *, /, **, %
+a = input("Podaj pierwsza wartosc: ")
+b = input("Podaj pierwsza wartosc: ")
+a = float(a)
+b = float(b)
+a += 3
+print(a)
+b -= 4 + 2
+print(b)
+a *= b
+print(a)
+b /= 2
+a **= 3
+print(a)
+a %= 15
+print(a)
