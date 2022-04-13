3a037bcf4f0641dc33f18e0ae93eb67f9095d722
zbrzezna-wioleta <100282558+zbrzezna-wioleta@users.noreply.github.com>
2022-04-13 11:53:36 +0200

    Add files via upload

---
+++ labolatorium8/main.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+# a = np.array([20, 30, 40, 50])
+# b = np.array(4)
+#
+# c= a+b
+# print(c)
+# print(b**2)
+# print(np.exp(b))
+# print(np.sqrt(b))
+# d= np.sqrt(b)
+# e=d+b
+# print(e)
+#
+# a=np.arange(12).reshape((3,4))
+# print(a)
+# print(" ")
+# # print(a.sum())
+# # print(a.sum(axis=0))
+# # print(a.sum(axis=1))
+# print(a.cumsum(axis=1))
+
+#
+# a = np.arange(3)
+# b = np.arange(3)
+# print(a.dot(b))
+#
+# c= np.array([[2, 3, 6], [5, 1, 3]])
+# d= np.array([[1, 4], [2, 1], [3, 5]])
+# e=c.dot(d)
+# print(e)
+
+# a = np.arange(6).reshape(3, 2)
+# print(a)
+# for b in a:
+#     for c in b:
+#         print(c)
+#
+# for b in a.flat:
+#     print(b)
+#
+# a = np.arange(6)
+# print(a)
+# b = a.reshape((3,2))
+# c = a. reshape((2, 3))
+# print(b)
+# print(c)
+# d = c.ravel()
+# print(d)
+# e=c.T
+# print(e)
